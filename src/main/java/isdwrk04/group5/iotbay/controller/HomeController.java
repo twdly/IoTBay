d21cfa1b9603f7bcbffb41091759b1cadfe6cd50
twdly <taiwoodley@gmail.com>
2024-03-23 19:26:34 +1100

    Fix HomeController intercepting js and css files

--- src/main/java/isdwrk04/group5/iotbay/controller/HomeController.java
+++ src/main/java/isdwrk04/group5/iotbay/controller/HomeController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "homeController", value = "/")
+@WebServlet(name = "homeController", value = "")
 public class HomeController extends BaseServlet {
 
     public void init() {
