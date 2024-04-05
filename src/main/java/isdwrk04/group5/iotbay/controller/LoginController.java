563262165871a126af1ce5324cb2886b67f371fc
channy <chandana.gangishetty@student.uts.edu.au>
2024-04-05 14:26:05 +1100

    Implement loginPage

--- src/main/java/isdwrk04/group5/iotbay/controller/LoginController.java
+++ src/main/java/isdwrk04/group5/iotbay/controller/LoginController.java
@@ -1,12 +1,14 @@
 package isdwrk04.group5.iotbay.controller;
 
 import isdwrk04.group5.iotbay.dao.UserDao;
+import isdwrk04.group5.iotbay.model.User;
 import isdwrk04.group5.iotbay.service.HashingService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
@@ -32,7 +34,22 @@ public class LoginController extends BaseServlet {
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("Email");
+        String password = request.getParameter("Password");
+
+        User user = userDao.getUserByEmail(email);
+        if (user != null) {
+            HttpSession session = request.getSession();
+            if (hashingService.checkPassword(user, password)) {
+                session.setAttribute("user", user);
+                serveJSP(request, response, "welcome.jsp");
+            }
+            response.sendRedirect("welcome.jsp");
+        } else {
+            request.setAttribute("errors", "Invalid email or password");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
     }
 
     @Override
--- src/main/webapp/WEB-INF/login.jsp
+++ src/main/webapp/WEB-INF/login.jsp
@@ -1,10 +1,10 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Login - IoTBay</title>
     </head>
-
     <body>
         <h1>Log in to your account</h1>
         <br>
