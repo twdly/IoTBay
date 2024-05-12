5f3e0d7ff1e822f2b97806f3bfb5929c73b0f07f
Pragya Roy <pragyaparamita.roy@gmail.com>
2024-05-12 13:31:50 +1000

    Updated AccessLogDAO to include methods to insert access logs

--- src/main/java/isdwrk04/group5/iotbay/dao/AccessLogDAO.java
+++ src/main/java/isdwrk04/group5/iotbay/dao/AccessLogDAO.java
@@ -15,6 +15,35 @@ public class AccessLogDAO {
         connection = new DbConnector().getConnection();
     }
 
+    public void insertLog(AccessLog log) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("insert into \"ACCESSLOG\" VALUES (" + buildQuery(log) + ")");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String buildQuery(AccessLog log) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(getNextLogId()).append(", ");
+        builder.append("'").append(log.getUserId()).append("', ");
+        builder.append("'").append(log.getEvent()).append("', ");
+        builder.append("'").append(log.getEventTime()).append("', ");
+        return builder.toString().trim();
+    }
+
+    private int getNextLogId() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("select max(LOG_ID) as ID from \"ACCESSLOG\"");
+            result.next();
+            return result.getInt("ID") + 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public List<AccessLog> getAllLogs() {
         List<AccessLog> logs = new ArrayList<>();
         try {
@@ -29,7 +58,7 @@ public class AccessLogDAO {
         return logs;
     }
 
-    public AccessLog createAccessLogFromResult(ResultSet result) throws SQLException {
+    private AccessLog createAccessLogFromResult(ResultSet result) throws SQLException {
         int logId = result.getInt("Log_ID");
         int userId = result.getInt("User_ID");
         String event = result.getString("Event");
