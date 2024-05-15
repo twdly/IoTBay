6df26884a9a1f7288fffef4a43db7bd7674643b4
Pragya Roy <pragyaparamita.roy@gmail.com>
2024-05-15 22:20:38 +1000

    Fixed functions for form validation and deactivation

--- src/main/webapp/js/update-account.js
+++ src/main/webapp/js/update-account.js
@@ -1,6 +1,5 @@
 function updateDetails() {
     let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
     let phone = document.getElementById("phoneNumber").value;
 
     let errorList = ""
@@ -8,17 +7,22 @@ function updateDetails() {
 
     if (email.indexOf('@') === -1) {
         isValid = false;
-        errorList += "Email is not valid<br/>"
+        errorList += "Email is not valid<br/>";
     }
-    if (!phone.matches("\\d{10}") || phone.length() != 10) {
+    if (!phone.match(/^\d{10}$/)) {
         isValid = false;
-        errorList += "Phone number must be a 10-digit number.<br/>"
+        errorList += "Phone number must be a 10-digit number<br/>";
+    }
+
+    if (phone.length !== 10) {
+        isValid = false;
+        errorList += "Phone number must be a 10-digit number<br/>";
     }
 
     if (isValid) {
-        document.getElementById("accountForm").submit()
+        document.getElementById("accountForm").submit();
     } else {
-        document.getElementById("errors").innerHTML = errorList
+        document.getElementById("errors").innerHTML = errorList;
     }
 }
 
@@ -31,20 +35,16 @@ function updatePassword() {
 
     if (password !== passwordCheck) {
         isValid = false;
-        errorList += "Passwords do not match<br/>"
+        errorList += "Passwords do not match<br/>";
     }
     if (password.length < 8) {
         isValid = false;
-        errorList += "Password is less than 8 characters<br/>"
+        errorList += "Password is less than 8 characters<br/>";
     }
 
     if (isValid) {
-        document.getElementById("deactivateForm").submit()
+        document.getElementById("deactivateForm").submit();
     } else {
-        document.getElementById("errors").innerHTML = errorList
+        document.getElementById("errors").innerHTML = errorList;
     }
-}
-
-function deactivateAccount() {
-    document.getElementById("deactivateForm").submit();
 }
