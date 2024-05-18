9f62f97537bf3cbb58322c8957201eb4fe9f1040
Pragya Roy <pragyaparamita.roy@gmail.com>
2024-05-19 00:49:57 +1000

    Fixed testUpdatePassword()

--- src/test/web/AccountManagementTest.java
+++ src/test/web/AccountManagementTest.java
@@ -19,13 +19,15 @@ public class AccountManagementTest extends BaseWebTest{
     @BeforeEach
     public void before() {
         userDao = new UserDao();
+//        Clean up test data before each test
         userDao.deleteUser("test@gmail.com");
         userDao.deleteUser("update@gmail.com");
         userDao.deleteUser("deleted");
     }
 
+    //    Helper method to create a new account for testing
     public void createAccount() {
-        driver.findElements(By.className("register-or-login")).get(0).click();
+        driver.findElement(By.linkText("Register")).click();
         driver.findElement(By.name("email")).sendKeys("test@gmail.com");
         driver.findElement(By.name("firstname")).sendKeys("first");
         driver.findElement(By.name("lastname")).sendKeys("second");
@@ -41,6 +43,7 @@ public class AccountManagementTest extends BaseWebTest{
     public void testUpdateDetails() {
         createAccount();
 
+//        Update account details
         driver.findElement(By.name("firstname")).clear();
         driver.findElement(By.name("firstname")).sendKeys("updated");
         driver.findElement(By.name("lastname")).clear();
@@ -52,6 +55,7 @@ public class AccountManagementTest extends BaseWebTest{
 
         driver.findElement(By.xpath("//button[text()='Update details']")).click();
 
+//        Verify that the account details were updated successfully
         User user = userDao.getUserByEmail("update@gmail.com");
 
         Assertions.assertEquals("updated", user.getUsername().split(" ")[0]);
@@ -64,33 +68,42 @@ public class AccountManagementTest extends BaseWebTest{
     public void testUpdatePassword() {
         createAccount();
 
-        User userBeforeUpdate = userDao.getUserByEmail("test@gmail.com");
-
-        driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys("updated");
-        driver.findElement(By.name("passwordCheck")).clear();
-        driver.findElement(By.name("passwordCheck")).sendKeys("updated");
+//        Change account password
+        driver.findElement(By.name("password")).sendKeys("updatedPassword");
+        driver.findElement(By.name("passwordCheck")).sendKeys("updatedPassword");
 
         driver.findElement(By.xpath("//button[text()='Change password']")).click();
 
-        User userAfterUpdate = userDao.getUserByEmail("test@gmail.com");
+//        Verify that the account can be accessed by the new password
+        driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.name("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("updatedPassword");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
 
-        Assertions.assertNotEquals(userBeforeUpdate.getHashedPassword(), userAfterUpdate.getHashedPassword());
+        String message = driver.findElement(By.className("welcome")).getText();
+        Assertions.assertTrue(message.contains("Welcome first second"));
+        Assertions.assertTrue(message.contains("Your email is test@gmail.com"));
     }
 
     @Test
     public void testDeleteAccount() {
         createAccount();
 
+//        Delete the account
         driver.findElement(By.xpath("//button[text()='Deactivate Account']")).click();
         driver.switchTo().alert().accept();
+//        Wait for the account deletion has been completed
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
         wait.until(ExpectedConditions.urlToBe("http://localhost:8080/IoTBay_war_exploded/"));
+
+//        Verify the account was deleted successfully
         Assertions.assertNull(userDao.getUserByEmail("test@gmail.com"));
     }
 
     @AfterEach
     public void after() {
+//        Clean up test data after each test
         userDao.deleteUser("test@gmail.com");
         userDao.deleteUser("update@gmail.com");
         userDao.deleteUser("deleted");
